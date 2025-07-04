c0ce2e6acc25049bfe3659139b73ae2802f0245d
Emma <you@example.com>
2025-07-04 09:57:01 +0200

    Dto done

---
+++ MovieApi/Models/DTOs/ActorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieApi.Models.DTOs
+{
+    public class ActorDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public int BirthYear { get; set; }
+    }
+}
---
+++ MovieApi/Models/DTOs/MovieDetailDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models.DTOs
+{
+    public class MovieDetailDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+
+        public string GenreName { get; set; } = string.Empty;
+
+        public string Synopsis { get; set; } = string.Empty;
+
+        public string Language { get; set; } = string.Empty;
+
+        public int Budget { get; set; }
+
+        public ICollection<ReviewDto> ReviewDtos { get; set; } = new List<ReviewDto>();
+
+        public ICollection<ActorDto> ActorDtos { get; set; } = new List<ActorDto>();
+    }
+}
