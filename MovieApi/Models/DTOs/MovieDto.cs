b5cbe4c43604eff9140bea5b249c6fc5c8d611d6
Emma <you@example.com>
2025-07-04 09:45:54 +0200

    created moviedto

---
+++ MovieApi/Models/DTOs/MovieDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models.DTOs
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+
+        public string GenreName { get; set; } = string.Empty;
+    }
+}
