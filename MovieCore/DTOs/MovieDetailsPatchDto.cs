3e1c5f5dc26727e5aadc04be6745e129769ddad9
Emma <you@example.com>
2025-07-17 14:58:15 +0200

    created dto for patch

---
+++ MovieCore/DTOs/MovieDetailsPatchDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieCore.DTOs
+{
+    public class MovieDetailsPatchDto
+    {
+        public string? Language { get; set; }
+        public string? Synopsis { get; set; }
+        public int? Budget { get; set; }
+    }
+
+
+}
---
+++ MovieCore/DTOs/MoviePatchDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCore.DTOs
+{
+    public class MoviePatchDto
+    {
+        public string? Title { get; set; }
+        public int? Year { get; set; }
+        public int? Duration { get; set; }
+
+        public MovieDetailsPatchDto MovieDetails { get; set; } = new();
+    }
+
+
+}
